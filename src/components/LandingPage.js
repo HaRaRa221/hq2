47392f9ca848983932c716077c90a268e0e40b37
HaRaRa221 <113466996+HaRaRa221@users.noreply.github.com>
2024-04-12 18:18:44 -0400

    updated landing page?

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,10 +2,30 @@
 import React from 'react';
 
 function LandingPage({ user }) {
+
+  const handleClick = () => {
+    console.log('Button clicked');
+  }
   return (
     <div>
       <h2>Welcome, {user}!</h2>
       <p>This is your landing page.</p>
+      <button onClick={handleClick}>Click this button</button>
+      <>
+        <h2>Another heading</h2>
+        <p>Another paragraph</p>
+        <form>
+          <input type="text" placeholder="Enter something" />
+          <button onClick={handleClick}>Submit</button>
+        </form>
+
+        <li>Achievements</li>
+        <li>Settings</li>
+        <li>Logout</li>
+        <ul>Challenges</ul>
+
+        
+      </>
     </div>
   );
 }
